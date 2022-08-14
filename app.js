ba516bd6a63ac9f9dd8461ae86a18182059fbf95
kiko <suin@gimboin-ui-MacBook.local>
2022-08-14 22:13:33 +0900

    recruit router, db router 추가

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dbRouter = require('./routes/db');
+var recruitRouter = require('./routes/recruit');
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/db', dbRouter);
+app.use('/recruit', recruitRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/db.js
+++ routes/db.js
@@ -9,15 +9,15 @@ const maria = require('../db/connect/maria');
  */
 
 // 채용 RECRUIT 테이블을 생성합니다.
-router.get('/db/recruit/create', function(req, res) {
-  var sql = 'CREATE TABLE RECRUIT ('
-  sql += 'RECRUIT_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
+router.get('/recruit/create', function(req, res) {
+  var sql = 'CREATE TABLE RECRUIT('
+  sql += 'RECRUIT_ID INT AUTO_INCREMENT PRIMARY KEY, '
   sql += 'COMPANY_ID INT(20) NOT NULL, '
   sql += 'POSITION VARCHAR(100) NOT NULL, '
   sql += 'COMPENSATION INT(15) NOT NULL, '
-  sql += 'CONTENT CLOB NOT NULL, '
+  sql += 'CONTENT LONGTEXT NOT NULL, '
   sql += 'SKILLS VARCHAR(100) NOT NULL '
-  sql += ')DEFAULT CHARSET=utf8 '
+  sql += ') DEFAULT CHARSET=utf8 '
 
   maria.query(sql, function(err, rows, fields) {
       if(!err) {
@@ -31,7 +31,7 @@ router.get('/db/recruit/create', function(req, res) {
 });
 
 // 채용 RECRUIT 테이블을 삭제합니다.
-router.get('/db/recruit/drop', function(req, res) {
+router.get('/recruit/drop', function(req, res) {
   maria.query('DROP TABLE RECRUIT', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
@@ -44,9 +44,9 @@ router.get('/db/recruit/drop', function(req, res) {
 });
 
 // 채용 RECRUIT 테이블에 데이터를 추가합니다.
-router.get('/db/recruit/insert', function(req, res) {
+router.get('/recruit/insert', function(req, res) {
   maria.query('INSERT INTO RECRUIT(COMPANY_ID,POSITION,COMPENSATION,CONTENT,SKILLS)'
-  +'VALUES("백엔드 주니어 개발자", 1000000, "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..", "Python")', function(err, rows, fields) {
+  +'VALUES(1, "백엔드 주니어 개발자", 1000000, "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..", "Python")', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
       } else {
@@ -58,7 +58,7 @@ router.get('/db/recruit/insert', function(req, res) {
 });
 
 // 채용 RECRUIT 테이블에서 데이터를 전체 조회합니다.
-router.get('/db/recruit/select', function(req, res) {
+router.get('/recruit/select', function(req, res) {
   maria.query('SELECT * FROM RECRUIT', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
@@ -71,7 +71,7 @@ router.get('/db/recruit/select', function(req, res) {
 });
 
 // 채용 RECRUIT 테이블에서 전체 데이터를 삭제합니다.
-router.get('/db/recruit/delete', function(req, res) {
+router.get('/recruit/delete', function(req, res) {
   maria.query('DELETE FROM RECRUIT', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
@@ -89,13 +89,13 @@ router.get('/db/recruit/delete', function(req, res) {
  */
 
 // 회사 COMPANY 테이블을 생성합니다.
-router.get('/db/company/create', function(req, res) {
+router.get('/company/create', function(req, res) {
   var sql = 'CREATE TABLE COMPANY ('
-  sql += 'COMPANY_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
+  sql += 'COMPANY_ID INT AUTO_INCREMENT PRIMARY KEY, '
   sql += 'COMPANY_NAME VARCHAR(50) NOT NULL, '
   sql += 'COUNTRY VARCHAR(50) NOT NULL, '
   sql += 'REGION VARCHAR(50) NOT NULL'
-  sql += ')DEFAULT CHARSET=utf8 ';
+  sql += ') DEFAULT CHARSET=utf8 ';
 
   maria.query(sql, function(err, rows, fields) {
       if(!err) {
@@ -109,7 +109,7 @@ router.get('/db/company/create', function(req, res) {
 });
 
 // 회사 COMPANY 테이블을 삭제합니다.
-router.get('/db/company/drop', function(req, res) {
+router.get('/company/drop', function(req, res) {
   maria.query('DROP TABLE COMPANY', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
@@ -122,7 +122,7 @@ router.get('/db/company/drop', function(req, res) {
 });
 
 // 회사 COMPANY 테이블에 데이터를 추가합니다.
-router.get('/db/company/insert', function(req, res) {
+router.get('/company/insert', function(req, res) {
   maria.query('INSERT INTO COMPANY(COMPANY_NAME,COUNTRY,REGION)'
   +'VALUES("원티드랩", "한국", "서울")', function(err, rows, fields) {
       if(!err) {
@@ -136,7 +136,7 @@ router.get('/db/company/insert', function(req, res) {
 });
 
 // 회사 COMPANY 테이블에서 데이터를 전체 조회합니다.
-router.get('/db/company/select', function(req, res) {
+router.get('/company/select', function(req, res) {
   maria.query('SELECT * FROM COMPANY', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
@@ -149,7 +149,7 @@ router.get('/db/company/select', function(req, res) {
 });
 
 // 회사 COMPANY 테이블에서 전체 데이터를 삭제합니다.
-router.get('/db/company/delete', function(req, res) {
+router.get('/company/delete', function(req, res) {
   maria.query('DELETE FROM COMPANY', function(err, rows, fields) {
       if(!err) {
         res.send(rows); // response send rows
